d10d8edcb231923d863dcb323ba14af211c3b7a2
Donny <1021001390@qq.com>
2017-03-22 17:48:17 +0800

    create project

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Donny on 17/3/22.
+ */
+(function () {
+    'use strict';
+
+    angular.module('seed', [
+        'seed.routers',
+        'seed.directives',
+        'seed.services',
+        'seed.controllers'
+    ]);
+
+}());
---
+++ js/routers/router.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Donny on 17/3/22.
+ */
+(function () {
+    'use strict';
+
+    angular.module('seed.routers', ['ui.router'])
+        .config(function ($urlRouterProvider, $stateProvider) {
+            $urlRouterProvider.otherwise('/app/home');
+
+            $stateProvider
+                .state('app', {
+                    abstract: true,
+                    url: '/app',
+                    templateUrl: './tpls/menu.html',
+                    controller: 'AppController'
+                })
+                .state('app.home', {
+                    url: '/home',
+                    templateUrl: './tpls/home.html',
+                    controller: 'HomeController'
+                })
+                .state('app.message', {
+                    url: '/message',
+                    templateUrl: './tpls/message.html',
+                    controller: 'MessageController'
+                })
+                .state('app.settings', {
+                    url: '/settings',
+                    templateUrl: './tpls/settings.html',
+                    controller: 'SettingsController'
+                });
+        });
+
+}());
